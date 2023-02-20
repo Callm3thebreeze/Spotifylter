b40e82b27915422bb3d694ef55fb03c53350c73c
callm3thebreeze <jesusvillarjimenez@gmail.com>
2023-02-20 20:38:24 +0100

    división etiquetado

--- spotify-api/src/App.jsx
+++ spotify-api/src/App.jsx
@@ -31,36 +31,44 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className='main-title'>Spotyfilter</h1>
-      <label className='search-input'>
-        Busca aquí:
-        <input 
-          value={searchInput} 
-          onChange={e => setSearchInput(e.target.value)} 
-          onKeyDown={ e => {
-            if(e.key=='Enter') {
-              search();
-            }
-          }}
-          />
-          <button onClick={() => { search()}}>
-            Buscar
-          </button>
-      </label>
-      <div className="albums-container">
-      {albumsShowed.length > 0 && albumsShowed.map( (album, i) =>{
 
-        return (
-            <div className="album-card" key={album.id}>
-              <img className="album-image" src={album.images[0].url} alt="" />
-                <p className="album-title">
-                  {album.name}
-                </p>
-            </div>
-        )
-      })}
-      {albumsShowed.length === 0 && <h1>No hay resultados para esta búsqueda</h1>}
-      </div>
+      <header>
+        <h1 className='main-title'>Spotyfilter</h1>
+        <label className='search-input'>
+          Busca aquí:
+          <input 
+            value={searchInput} 
+            onChange={e => setSearchInput(e.target.value)} 
+            onKeyDown={ e => {
+              if(e.key=='Enter') {
+                search();
+              }
+            }}
+            />
+            <button type="submit" onClick={() => { search()}}>
+              Buscar
+            </button>
+        </label>
+      </header>
+
+      <main>
+        <div className="albums-container">
+        {albumsShowed.length > 0 && albumsShowed.map( (album, i) =>{
+
+          return (
+              <div className="album-card" key={album.id}>
+                <img className="album-image" src={album.images[0].url} alt="" />
+                  <p className="album-title">
+                    {album.name}
+                  </p>
+              </div>
+          )
+        })}
+        {albumsShowed.length === 0 && <h1>No hay  resultados para esta búsqueda</h1>}
+        </div>
+      </main>
+
+
     </div>
   )
 }
